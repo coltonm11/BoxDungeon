4482b2ab01d63de541e7912028a9e4f5fa9195af
coltonm <coltonmarshall@gmail.com>
2019-07-22 11:31:07 -0700

    Snap to grid

--- 1907 Box Dungeon/Assets/Scripts/GroundPlacementController.cs
+++ 1907 Box Dungeon/Assets/Scripts/GroundPlacementController.cs
@@ -6,6 +6,7 @@ public class GroundPlacementController : MonoBehaviour
 {
     bool placed;
     GameObject container;
+    public float gridSize;
 
     private void Start()
     {
@@ -22,13 +23,32 @@ public class GroundPlacementController : MonoBehaviour
     {
         if (!placed)
         {
-            Vector3 placementPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1);
-            container.transform.position = Camera.main.ScreenToWorldPoint(placementPosition);
+            container.transform.position = GetNearestPointToGrid();
             if (Input.GetMouseButtonDown(0))
             {
                 placed = true;
             }
         }
+    }
+
+    public Vector3 GetNearestPointToGrid()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1);
+        Vector3 mousePosWorld = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        int xCount = Mathf.RoundToInt(mousePosWorld.x / gridSize);
+        int yCount = Mathf.RoundToInt(mousePosWorld.y / gridSize);
+        int zCount = 1;
+
+
+        Vector3 result = new Vector3
+        (
+            xCount * gridSize,
+            yCount * gridSize,
+            zCount
+        );
+
+        return result;
     }
 
 
